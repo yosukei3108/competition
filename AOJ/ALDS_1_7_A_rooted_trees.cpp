8a0d7c11f70b378315c3d8bb04e8938a26499f67
yosukei3108 <38313011+yosukei3108@users.noreply.github.com>
2021-07-24 20:22:59 +0900

    pass ALDS_1_7_A

--- AOJ/ALDS_1_7_A_rooted_trees.cpp
+++ AOJ/ALDS_1_7_A_rooted_trees.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 #define MAX 100005
-#define NaN -100
+#define NaN -1
 
 struct Node { int p, l, r; };
 
@@ -10,17 +10,16 @@ Node T[MAX];
 int n, D[MAX];
 
 void print(int u) {
+    int i, c;
     cout << "node " << u << ": ";
-    cout << "parent" << T[u].p << ", ";
-    cout << "depth" << D[u] << ", ";
+    cout << "parent = " << T[u].p << ", ";
+    cout << "depth = " << D[u] << ", ";
 
     if(T[u].p == NaN) cout << "root, ";
     else if(T[u].l == NaN) cout << "leaf, ";
     else cout << "internal node, ";
 
     cout << "[";
-
-    int i, c;
 
     for(i=0, c=T[u].l; c!=NaN; i++, c=T[c].r) {
         if(i) cout << ", ";
@@ -29,68 +28,54 @@ void print(int u) {
     cout << "]" << endl;
 }
 
-int depth_rec(int u, int p) {
-    cout << "depth_rec()" << endl;
-    cout << "input: u = " << u << ", p = " << p << endl;
+void depth_rec(int u, int p) {
+    // cout << "into depth_rec()" << endl;
 
     D[u] = p;
-    
-    if(T[u].r != NaN) cout << "T[u].r should not be -100, but T[u].r is " << T[u].r << endl;
-    
-    
-    if(T[u].r != NaN) {
-        cout << "right sibling has found" << endl;
-        depth_rec(T[u].r, p);
-    } else {
-        cout << "right sibling has NOT found" << endl;
-    }
 
-    if(T[u].l != NaN) {
-        cout << "T[u].l should not be -100, but T[u].l is " << T[u].l << endl;
-        cout << "typeid(T[u].l).name() = " << typeid(T[u].l).name() << endl;
-        cout << "typeid(NaN).name() = " << typeid(NaN).name() << endl;
-    }
+    // cout << "D[u] = " << D[u] << endl;
 
-    if(T[u].l != NaN) {
-        cout << "left child has found" << endl;
-        depth_rec(T[u].l, p+1);
-    } else {
-        cout << "left child has NOT found" << endl;
-    }
+    // cout << "T[u].r = " << T[u].r << endl;
+    if(T[u].r != NaN) depth_rec(T[u].r, p);
+
+    // cout << "T[u].l = " << T[u].l << endl;
+    if(T[u].l != NaN) depth_rec(T[u].l, p+1);
 }
 
 int main() {
+
+    // cout << "start main()" << endl;
+
     int i, j, d, v, c, l, r;
     cin >> n;
-    
+
     for(i=0; i<n; i++) {
         T[i].p = NaN;
         T[i].l = NaN;
         T[i].r = NaN;
     }
 
+    // cout << "initialize T" << endl;
+    // cout << "T[0].p = " << T[0].p << endl;
+
     for(i=0; i<n; i++) {
         cin >> v >> d;
         for(j=0; j<d; j++) {
             cin >> c;
-            
-            if(j == 0) {
-                T[v].l = c;
-                cout << "T[v].l = " << T[v].l << endl;
-            } else {
-                T[l].r = c;
-                cout << "T[l].r = " << T[l].r << endl;
-            }
+            if(j == 0) T[v].l = c;
+            else T[l].r = c;
             l = c;
             T[c].p = v;
         }
     }
+
+    // cout << "read input" << endl;
+
     for(i=0; i<n; i++) {
-        if(T[i].p == NaN) {
-            r = i;
-            cout << "r has set as " << i << endl; 
-        }
+        if(T[i].p == NaN) r = i;
     }
+
+    // cout << "write r with i" << endl;
 
     depth_rec(r, 0);
 
