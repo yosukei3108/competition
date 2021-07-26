7805360c91748b1aed2dc212013de2de2117c44c
yosukei3108 <38313011+yosukei3108@users.noreply.github.com>
2021-07-26 23:03:00 +0900

    pass ALDS_1_8_A

--- AOJ/ALDS_1_8_A_binary_search_tree_1.cpp
+++ AOJ/ALDS_1_8_A_binary_search_tree_1.cpp
@@ -40,78 +40,39 @@ void insert(int k) {
 }
 
 void inorder(Node *u) {
+  if(u == NIL) return;
 
+  inorder(u->left);
+  cout << " " << u->key;
+  inorder(u->right);
 }
-
-/*
-void inParse(int u) {
-    // cout << "u = " << u << endl;
-    if(u == NIL) return;
-
-    inParse(T[u].left);
-
-    cout << " " << u;
-
-    inParse(T[u].right);
-}
-*/
-
-
 
 void preorder(Node *u) {
+  if(u == NIL) return;
 
+  cout << " " << u->key;
+  preorder(u->left);
+  preorder(u->right);
 }
-/*
-void preParse(int u) {
-    // cout << "u = " << u << endl;
-    if(u == NIL) return;
-
-    cout << " " << u;
-
-    preParse(T[u].left);
-    preParse(T[u].right);
-}
-*/
 
 int main() {
     int n, i, x;
-    string = com;
+    string com;
 
     cin >> n;
 
     for(int i=0; i<n; i++) {
-
+      cin >> com;
+      if(com == "insert") {
+        cin >> x;
+        insert(x);
+      } else if(com == "print") {
+        inorder(root);
+        cout << endl;
+        preorder(root);
+        cout << endl;
+      }
     }
-
-   //  cout << "initialize T" << endl;
-    // cout << "T[0].parent = " << T[0].parent << endl;
-
-    for(int i=0; i<n; i++) {
-        cin >> v >> l >> r;
-        T[v].left = l;
-        T[v].right = r;
-        if(l != NIL) T[l].parent = v;
-        if(r != NIL) T[r].parent = v;
-    }
-
-    // cout << "read input" << endl;
-
-    for(int i=0; i<n; i++) if(T[i].parent == NIL) root = i;
-
-    // cout << "write root with i" << endl;
-
-    cout << "Preorder" << endl;
-    preParse(root);
-    cout << endl;
-
-    cout << "Inorder" << endl;
-    inParse(root);
-    cout << endl;
-
-    cout << "Postorder" << endl;
-    postParse(root);
-    cout << endl;
 
     return 0;
-
 }
