346d96e6c60ee1f95c17efe9307ff90c7160e9d6
yosukei3108 <38313011+yosukei3108@users.noreply.github.com>
2024-01-14 08:42:56 +0900

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,7 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-  
+
+// 計算量の目安: 実行時間制限が 1 秒の場合
+// 1,000,000 -> 間に合う
+// 10,000,000 -> 概ね間に合う
+// 100,000,000 -> 間に合わない可能性が高い
+
 int main(void){
     int n, m;
     string s;
